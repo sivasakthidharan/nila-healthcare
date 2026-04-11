58b945df4544c9406682a9e5db8dd10d2f9663e4
siva <sivasakthihbk@gmail.com>
2026-04-11 18:56:08 +0530

    checekd

--- nila-backend/src/db.ts
+++ nila-backend/src/db.ts
@@ -4,12 +4,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const pool = new Pool({
-   //connectionString: process.env.DATABASE_URL,
-   user: process.env.DB_USER,
-   host: process.env.DB_HOST,
-   database: process.env.DB_NAME,
-   password: process.env.DB_PASSWORD,
-   port: Number(process.env.DB_PORT),
+   connectionString: process.env.DATABASE_URL,
+   
+    //  ssl: { rejectUnauthorized: false }  // ✅ Required for Supabase
+
+   
+         user: process.env.DB_USER,
+         host: process.env.DB_HOST,
+         database: process.env.DB_NAME,
+         password: process.env.DB_PASSWORD,
+         port: Number(process.env.DB_PORT),
   //newly added
 });
 
