bcdeca4fa29d6d3cbd8c968f8c301f620be37d0a
siva <sivasakthihbk@gmail.com>
2026-04-13 20:17:03 +0530

    fix

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -16,7 +16,7 @@ dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin:["http://nila-healthcare-4hb3wnonv-sivasakthidharans-projects.vercel.app",
+  origin:["http://nila-health-43caklq1f-sivasakthidharans-projects.vercel.app",
     "http://localhost:5173" ],
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
