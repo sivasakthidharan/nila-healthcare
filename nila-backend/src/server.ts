47165e87de38010f6ba8d6a25930c0764f509521
siva <sivasakthihbk@gmail.com>
2026-04-14 19:21:58 +0530

    adding expert

--- nila-backend/src/server.ts
+++ nila-backend/src/server.ts
@@ -206,6 +206,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 import authRoutes from "./routes/auth.routes";
+import expertRoutes from "./routes/expert.routes";
 
 dotenv.config();
 
@@ -215,6 +216,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/auth", authRoutes);
+app.use("/api/experts", expertRoutes);
 
 app.get("/", (req, res) => {
   res.send("Backend running ✅");
